0cd9d3d2c1813ebae255cb4aa0b2bc23070bcb33
matiasramonda15-rgb <matiasramonda15@gmail.com>
2026-04-22 20:19:26 -0300

    Update supabase.js

--- feedlot-app/src/supabase.js
+++ feedlot-app/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  'https://jmxulljagtgcszdxlebo.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpteHVsbGphZ3RnY3N6ZHhsZWJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODU4MTMsImV4cCI6MjA5MjM2MTgxM30.gjBB1v_maxBWx5qjH8seyN101AMCaNRzYIkqv48V_Vs'
+)
